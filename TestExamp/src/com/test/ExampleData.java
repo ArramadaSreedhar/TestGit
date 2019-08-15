827da8fcde7f36453df0bb967bde564d19aef3b6
Sreedhar <43245377+ArramadaSreedhar@users.noreply.github.com>
2019-08-15 14:07:47 +0530

    Commit data

--- TestExamp/src/com/test/ExampleData.java
+++ TestExamp/src/com/test/ExampleData.java
@@ -4,7 +4,7 @@ public class ExampleData {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Master Example Data....");
+		System.out.println("Master Example Data....Change1");
 	}
 
 }
